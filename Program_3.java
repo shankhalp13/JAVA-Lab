c85a7a97145a469858ff5a48394e257f85b7777c
shankhalp13 <shankhalppradhan2003@gmail.com>
2023-07-30 22:14:14 +0530

    Added Program to Display Date and Time using Date Class

---
+++ Program_3.java
@@ -0,0 +1,12 @@
+//Display date and time using date class
+
+import java.util.Date;
+
+class driver2
+{
+	public static void main(String args[])
+	{
+		Date D1 = new Date();
+		System.out.println("Date and Time:"+D1);
+	}
+}
